2985fa0eb988f03b02765cd65796e2507c9d72a7
weeksontheweb <andy@weeksontheweb.co.uk>
2024-11-11 21:59:23 +0000

    First commit.

---
+++ MyOrleansGrainInterfaces/IHello.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOrleansGrainInterfaces
+{
+    public interface IHello : IGrainWithIntegerKey
+    {
+        ValueTask<string> SayHello(string greeting);
+    }
+}
